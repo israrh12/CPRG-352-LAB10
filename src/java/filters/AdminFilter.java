590435925a8f75047d777641d91b2c34d7a96c1a
Israr Hussaini <israr.hussaini@edu.sait.ca>
2021-11-25 12:57:34 -0700

    completed lab10

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import models.User;
+
+
+public class AdminFilter implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest httpRequest = (HttpServletRequest)request;
+        HttpSession session= httpRequest.getSession();
+        User user = (User) session.getAttribute("user");
+        if (user.getRole().getRoleId() != 1) {
+            HttpServletResponse httpResponse = (HttpServletResponse) response;
+            httpResponse.sendRedirect("notes");
+            return;
+        }
+        
+        chain.doFilter(request, response);
+    }
+    
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+    
+}
